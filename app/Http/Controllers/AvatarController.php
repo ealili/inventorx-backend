4f7dbed764bdfbd471fba3ff9387ee9d37024a9e
Enes Alili <e.alili@ineting.net>
2023-10-13 17:47:19 +0200

    Cleanup AvatarController

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -13,23 +13,11 @@ class AvatarController extends Controller
             'avatar' => 'required|image',
         ]);
 
-        $avatarName = time() . '.' . $request->avatar->getClientOriginalExtension();
-
         $file = Storage::putFile('avatars', $request->avatar);
 
 
         Auth()->user()->update(['avatar' => env('APP_URL') . '/storage/' . $file]);
 
         return $file;
-
-        return asset('storage/' . $avatarName);
-
-//        $request->avatar->move(public_path('avatars'), $avatarName);
-
-        Auth()->user()->update(['avatar' => $avatarName]);
-
-        return asset('storage/' . $avatarName);
-
-        return response(['message' => 'Sucesss'], 200);
     }
 }
