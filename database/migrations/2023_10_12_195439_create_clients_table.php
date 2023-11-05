9c2394233565ec2599d8303b0f3ee36b9e749871
Enes Alili <enesalili00@gmail.com>
2023-11-05 19:10:32 +0100

    [WIP] Add relation between client & team

--- database/migrations/2023_10_12_195439_create_clients_table.php
+++ database/migrations/2023_10_12_195439_create_clients_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('company_name');
             $table->string('address');
             $table->string('website')->nullable();
+            $table->foreignId('team_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
