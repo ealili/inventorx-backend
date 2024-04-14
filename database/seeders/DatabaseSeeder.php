5d689cdd7c5e9c3fd8232ca9d6e66dbae4ca67a6
Enes Alili <enesalili00@gmail.com>
2024-04-14 20:44:06 +0200

    Test user login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Team;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,22 +27,26 @@ class DatabaseSeeder extends Seeder
         \App\Models\Project::factory(20)->create();
         \App\Models\Task::factory(20)->create();
 
-        \App\Models\User::factory()->create([
+        DB::table('users')->insert([
             'name' => 'Admin User',
             'email' => 'admin@mail.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'avatar' => env('APP_URL') . '/storage/avatars/' . 'default-profile-picture.jpeg',
             'role_id' => 1,
-            'team_id' => 1
+            'team_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'User',
             'email' => 'user@mail.com',
             'password' => bcrypt('password'),
             'avatar' => env('APP_URL') . '/storage/avatars/' . 'default-profile-picture.jpeg',
             'role_id' => 2,
-            'team_id' => 1
+            'team_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
